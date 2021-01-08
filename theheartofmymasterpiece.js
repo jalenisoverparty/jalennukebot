671ac72d2ae236981e03e9de728699e34bab3fe3
jalenisoverparty <77172697+jalenisoverparty@users.noreply.github.com>
2021-01-08 15:24:58 -0500

    Add files via upload

---
+++ theheartofmymasterpiece.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () =>{
+  console.log("prepare the jalen is lesbian nuke");
+});
+client.on('message', msg => {
+  if (msg.content = "ping"){
+    msg.channel.send("jalen is lesbian because he likes women");
+    msg.channel.send("Jalen? Whoa mama!:open_mouth::open_mouth: Hummina hummina hummina bazooooooooing:kissing_smiling_eyes:! eyes pop out :open_mouth::open_mouth::open_mouth::wink:AROOOOOOOOGA! jaw drops tongue rolls out :scream::scream::scream:WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF:dog::dog: tongue bursts out of the outh uncontrollably leaking face and everything in reach:stuck_out_tongue::stuck_out_tongue::stuck_out_tongue::stuck_out_tongue::stuck_out_tongue: WURBLWUBRLBWURblrwurblwurlbrwubrlwburlwbruwrlblwublr ,:zany_face::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::zany_face:tiny cupid shoots an arrow through heart :cupid: Ahhhhhhhhhhh me lady... heart in the shape of a heart starts beating so hard you can see it through shirt:heart_eyes::heart_eyes::heart_eyes: ba-bum ba-bum ba-bum ba-bum ba-bum :drum::drum::drum: milk truck crashes into a bakery store in the background spiling white liquid and dough on the streets :milk::cow2::milk:BABY WANTS TO FUCK :warning::warning::warning::warning::warning: inhales from the gas tank :fuelpump::fuelpump::fuelpump:honka honka honka honka :taxi::taxi:masturabtes furiously :zany_face::zany_face::zany_face::zany_face::zany_face::eggplant::eggplant::sweat_drops::sweat_drops::sweat_drops::sweat_drops:ohhhh my gooooodd~:open_mouth:");
+  }
+});
+
+client.login('Nzk3MTk1MTcxMzM4OTExNzk0.X_i73A.RKlSpZAZWR7E7xq8_z8c8t0Ngqo');
